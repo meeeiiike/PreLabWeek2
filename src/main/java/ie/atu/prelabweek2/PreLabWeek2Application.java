b65d25f33ed4d29430dcea6a2a633e33e4c6abae
meike <g00407095@atu.ie>
2025-09-19 18:15:41 +0100

    Set up Spring Boot Project

---
+++ src/main/java/ie/atu/prelabweek2/PreLabWeek2Application.java
@@ -0,0 +1,13 @@
+package ie.atu.prelabweek2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreLabWeek2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PreLabWeek2Application.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/prelabweek2/PreLabWeek2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.prelabweek2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreLabWeek2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
