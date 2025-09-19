01aa8c8b5ee809b93b376d3853138a4d9316c473
meike <g00407095@atu.ie>
2025-09-19 18:52:58 +0100

    Added some comments, created another get method which uses part of the url for the method parameter. With swagger ui we have a useful input box for tesing. this method takes a value back and stores in a variable which we simply print

--- src/main/java/ie/atu/prelabweek2/RequestController.java
+++ src/main/java/ie/atu/prelabweek2/RequestController.java
@@ -1,6 +1,7 @@
 package ie.atu.prelabweek2;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,13 +9,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping
 public class RequestController {
 
+    // Known text sent through /hello url
     @GetMapping("/hello")
     public String hello(){
         return "Hello";
     }
 
-    @GetMapping("/helloAlso")
-    public String helloAlso(){
-        return "HelloAlso";
+    // Using path variable, uses part of url for method
+    // parameter and will be sent back and stored in a variable
+    @GetMapping("/greet/{name}")
+    public String greetByName(@PathVariable String name){
+        return "Hello " + name;
     }
 }
